6f10d5698aa6efaedbcb2d67f79bb18707706470
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-12-08 06:53:01 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5495079265:AAHjhcna03KLCpc2qnYON49x2RCzV4Kn_O0')
+BOT_TOKEN =  os.environ.get('bot_token','5810126379:AAHx7R-OpSEud30mUz_gGH39T4NkA2tLX5w')
 API_ID =  os.environ.get('api_id','15472343')
 API_HASH = os.environ.get('api_hash','6817c82fe0b762ed91999f49cd17f7d7')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
