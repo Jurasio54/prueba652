fd7ead520101069732bb2f2e3697d80962ce44cf
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-12-08 06:45:10 -0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-Oimport os
+import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5810126379:AAHx7R-OpSEud30mUz_gGH39T4NkA2tLX5w')
+BOT_TOKEN =  os.environ.get('bot_token','5495079265:AAHjhcna03KLCpc2qnYON49x2RCzV4Kn_O0')
 API_ID =  os.environ.get('api_id','15472343')
 API_HASH = os.environ.get('api_hash','6817c82fe0b762ed91999f49cd17f7d7')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','demian2008').split(';')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KGDEKFYDJJJJGDYGJDGJGHYICKHDRGDDLHDJLG'))
+PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KKDDKFYDJHJEGFYDJDGIGDYDJEIJGERJCECDCDDE'))
 
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
